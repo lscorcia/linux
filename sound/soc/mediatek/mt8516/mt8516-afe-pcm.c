9b10729b69b39cc805b8ad55cf585d1157195d56
Fabien Parent <fparent@baylibre.com>
2019-05-02 14:10:41 +0200

    ASoC: mediatek: mt8516: Register ADDA DAI
    Register the ADDA DAI driver into the MT8516 PCM driver.
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

--- sound/soc/mediatek/mt8516/mt8516-afe-pcm.c
+++ sound/soc/mediatek/mt8516/mt8516-afe-pcm.c
@@ -10,6 +10,7 @@
 #include <linux/module.h>
 #include <linux/of.h>
 
+#include "mt8516-afe-common.h"
 #include "mt8516-afe-regs.h"
 
 #include "../common/mtk-afe-platform-driver.h"
@@ -662,6 +663,7 @@ static int mt8516_dai_memif_register(struct mtk_base_afe *afe)
 
 typedef int (*dai_register_cb)(struct mtk_base_afe *);
 static const dai_register_cb dai_register_cbs[] = {
+	mt8516_dai_adda_register,
 	mt8516_dai_memif_register,
 };
 
