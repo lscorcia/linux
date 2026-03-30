730a372f834bf228463c31b79e929b8fbfe38b31
Luca Leonardo Scorcia <l.scorcia@gmail.com>
2026-03-30 22:08:52 +0100

    mediatek: connectivity: Switch to ktime helpers / timespec64

--- drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/gl_kal.c
+++ drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/gl_kal.c
@@ -3934,10 +3934,10 @@ int kalMetRemoveProcfs(void)
 #endif
 UINT_64 kalGetBootTime(void)
 {
-	struct timespec ts;
+	struct timespec64 ts;
 	UINT_64 bootTime = 0;
 
-	get_monotonic_boottime(&ts);
+	ktime_get_boottime_ts64(&ts);
 	/*
 	 * we assign ts.tv_sec to bootTime first, then multiply USEC_PER_SEC
 	 * this will prevent multiply result turn to a negative value on 32bit system
