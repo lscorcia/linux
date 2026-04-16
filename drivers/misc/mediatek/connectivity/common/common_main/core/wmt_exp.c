83ffcb6970f2d8a5570cf92c080fb236a489d7cf
Luca Leonardo Scorcia <l.scorcia@gmail.com>
2026-04-16 07:53:52 +0100

    [WIP] mediatek: Crank up the debug level

--- drivers/misc/mediatek/connectivity/common/common_main/core/wmt_exp.c
+++ drivers/misc/mediatek/connectivity/common/common_main/core/wmt_exp.c
@@ -55,7 +55,7 @@ wmt_wlan_bus_cnt_clr_cb mtk_wcn_wlan_bus_tx_cnt_clr;
 OSAL_BIT_OP_VAR gBtWifiGpsState;
 OSAL_BIT_OP_VAR gGpsFmState;
 UINT32 gWifiProbed;
-UINT32 gWmtDbgLvl = WMT_LOG_INFO;
+UINT32 gWmtDbgLvl = WMT_LOG_LOUD;
 MTK_WCN_BOOL g_pwr_off_flag = MTK_WCN_BOOL_TRUE;
 
 /*******************************************************************************
