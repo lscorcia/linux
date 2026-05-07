bd120c2dfebc15cd8a441046964d4ab597374e0b
Luca Leonardo Scorcia <l.scorcia@gmail.com>
2026-05-07 21:32:21 +0100

    watchdog: mediatek: Add wdt/toprgu resets for MT6589
    According to Android sources, MT6589 has 12 reset bits in the
    WDT_SWSYSRST register. Populate toprgu_sw_rst_num to allow toprgu resets
    in device trees of the many compatible devices.
    
    Signed-off-by: Luca Leonardo Scorcia <l.scorcia@gmail.com>

--- drivers/watchdog/mtk_wdt.c
+++ drivers/watchdog/mtk_wdt.c
@@ -10,6 +10,7 @@
  */
 
 #include <dt-bindings/reset/mt2712-resets.h>
+#include <dt-bindings/reset/mediatek,mt6589-resets.h>
 #include <dt-bindings/reset/mediatek,mt6735-wdt.h>
 #include <dt-bindings/reset/mediatek,mt6795-resets.h>
 #include <dt-bindings/reset/mt7986-resets.h>
@@ -86,6 +87,10 @@ struct mtk_wdt_data {
 
 static const struct mtk_wdt_data mt2712_data = {
 	.toprgu_sw_rst_num = MT2712_TOPRGU_SW_RST_NUM,
+};
+
+static const struct mtk_wdt_data mt6589_data = {
+	.toprgu_sw_rst_num = MT6589_TOPRGU_SW_RST_NUM,
 };
 
 static const struct mtk_wdt_data mt6735_data = {
@@ -493,7 +498,7 @@ static int mtk_wdt_resume(struct device *dev)
 
 static const struct of_device_id mtk_wdt_dt_ids[] = {
 	{ .compatible = "mediatek,mt2712-wdt", .data = &mt2712_data },
-	{ .compatible = "mediatek,mt6589-wdt" },
+	{ .compatible = "mediatek,mt6589-wdt", .data = &mt6589_data },
 	{ .compatible = "mediatek,mt6735-wdt", .data = &mt6735_data },
 	{ .compatible = "mediatek,mt6795-wdt", .data = &mt6795_data },
 	{ .compatible = "mediatek,mt7986-wdt", .data = &mt7986_data },
